afeb8cd659bcf9552e6589128d037983e881133e
Mohammmad Aziz <mdaziz067@gmail.com>
2017-10-22 08:16:20 +0530

    Adding assertion for user saved successfully

--- test/create_test.js
+++ test/create_test.js
@@ -2,8 +2,11 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('save a user', () => {
+  it('save a user', done => {
     const aziz = new User({ name: 'Aziz' });
-    aziz.save();
+    aziz.save().then(() => {
+      assert(!aziz.isNew);
+      done();
+    });
   });
 });
